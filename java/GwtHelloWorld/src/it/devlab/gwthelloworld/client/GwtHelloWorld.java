442c2cac12d00d04b308e12166b3d4bbcc8483cb
gianmarco.gherardi <devnull@localhost>
2009-12-13 20:30:40 +0100

    GWT hacking

--- java/GwtHelloWorld/src/it/devlab/gwthelloworld/client/GwtHelloWorld.java
+++ java/GwtHelloWorld/src/it/devlab/gwthelloworld/client/GwtHelloWorld.java
@@ -4,7 +4,6 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 public class GwtHelloWorld implements EntryPoint {
-
 	public void onModuleLoad() {
 		RootLayoutPanel.get().add(new MainComposite());
 	}
--- java/GwtHelloWorld/src/it/devlab/gwthelloworld/client/MainComposite.java
+++ java/GwtHelloWorld/src/it/devlab/gwthelloworld/client/MainComposite.java
@@ -5,9 +5,13 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.MenuItem;
+import com.google.gwt.user.client.ui.PushButton;
+import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeItem;
 
 public class MainComposite extends Composite {
 	interface MainUiBinder extends UiBinder<DockLayoutPanel, MainComposite> {
@@ -24,6 +28,12 @@ public class MainComposite extends Composite {
 	@UiField
 	MenuItem menuItem122;
 
+	@UiField
+	Tree tree;
+
+	@UiField
+	DockLayoutPanel dock;
+
 	public MainComposite() {
 		initWidget(uiBinder.createAndBindUi(this));
 		Command cmd = new Command() {
@@ -34,5 +44,20 @@ public class MainComposite extends Composite {
 		menuItem11.setCommand(cmd);
 		menuItem121.setCommand(cmd);
 		menuItem122.setCommand(cmd);
+
+		TreeItem root = new TreeItem("root");
+		root.addItem("item0");
+		root.addItem("item1");
+		root.addItem("item2");
+
+		// Add a CheckBox to the tree
+		TreeItem item = new TreeItem(new CheckBox("item3"));
+		root.addItem(item);
+
+		tree.addItem(root);
+		
+		new PushButton().setWidth(width)
+//		dock.setSize("7em", "2em");
+		
 	}
 }
