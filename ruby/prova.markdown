beac72ecfec62cbccd3cb38686f17230a952ae61
gianmarco.gherardi <devnull@localhost>
2010-02-07 11:49:01 +0100

    ruby wiki

---
+++ ruby/prova.markdown
@@ -0,0 +1,15 @@
+| First Header  | Second Header |
+| ------------- | ------------- |
+| Content Cell  | Content Cell  |
+| Content Cell  | Content Cell  |
+
+
+Example of code block
+
+
+~~~
+
+blank line before
+blank line after
+
+~~~
--- ruby/wiki.rb
+++ ruby/wiki.rb
@@ -2,23 +2,39 @@ require 'rubygems'
 require 'webrick'
 require 'kramdown'
 
-DOCUMENT_ROOT = Dir.pwd
-WEBSERVER_PORT = 2000
-
-class WikiServlet < WEBrick::HTTPServlet::AbstractServlet
-	def do_GET(req, res)
-		file_path = DOCUMENT_ROOT + req.path
-		raise WEBrick::HTTPStatus::NotFound unless File.exist?(file_path)
-		res['content-type'] = 'text/html'
-		File.open(file_path, 'r') do |f|
-			res.body = Kramdown::Document.new(f.read).to_html
+module Wiki
+	class WikiServlet < WEBrick::HTTPServlet::AbstractServlet
+		def do_GET(req, res)
+			file_path = File.join(@server[:DocumentRoot], req.path)
+			raise WEBrick::HTTPStatus::NotFound unless File.exist?(file_path)
+			res['content-type'] = 'text/html'
+			res.body = Wiki.to_html(file_path)
 		end
+	end
+
+	def self.to_html(file)
+		html = ''
+		File.open(File.join(File.dirname(__FILE__), 'page_template.html')) { |f| html = f.read }
+		File.open(file) do	|f| 
+			html.gsub!(/<!--\s*content\s*-->/, Kramdown::Document.new(f.read).to_html) 
+		end
+		html
+	end
+	
+	def self.export(target_dir, document_root = Dir.pwd)
+		
+	end
+
+	def self.start_server(port = 2000, document_root = Dir.pwd)
+		WEBrick::HTTPServlet::FileHandler.add_handler 'markdown', Wiki::WikiServlet
+		server = WEBrick::HTTPServer.new(:Port => port, :DocumentRoot => document_root)
+		['INT', 'TERM'].each do |signal|
+			trap(signal) { server.shutdown }
+		end
+		server.start
 	end
 end
 
-WEBrick::HTTPServlet::FileHandler.add_handler 'wiki', WikiServlet
-server = WEBrick::HTTPServer.new(:Port => WEBSERVER_PORT, :DocumentRoot => DOCUMENT_ROOT)
-['INT', 'TERM'].each do |signal|
-	trap(signal) { server.shutdown }
+if $0 == __FILE__
+	Wiki.start_server
 end
-server.start
