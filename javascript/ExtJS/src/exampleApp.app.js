ac366960706649439d621d538c749ad509b9b81e
gianmarco.gherardi <devnull@localhost>
2010-02-17 22:05:39 +0100

    ExtJS

--- javascript/ExtJS/src/exampleApp.app.js
+++ javascript/ExtJS/src/exampleApp.app.js
@@ -1,6 +1,20 @@
 Ext.namespace('exampleApp');
 
 exampleApp.app = function() {
+	var tabPanel = new Ext.TabPanel( {
+		flex : 1,
+		activeTab : 0,
+		border : false,
+		items : [ {
+			title : 'Tab 1',
+			html : '<p>Center</p>'
+		}, {
+			title : 'Tab 2',
+			closable : true,
+			html : '<p>Center2</p>'
+		} ]
+	});
+
 	new Ext.Viewport( {
 		layout : {
 			type : 'vbox',
@@ -15,35 +29,22 @@ exampleApp.app = function() {
 					xtype : 'menu',
 					items : [ {
 						text : 'Open..',
-						handler: exampleApp.menuClickHandler
+						handler : exampleApp.menuClickHandler
 					}, {
 						text : 'Save',
-						handler: exampleApp.menuClickHandler
+						handler : exampleApp.menuClickHandler
 					}, '-', {
 						text : 'Exit',
-						handler: exampleApp.menuClickHandler
+						handler : exampleApp.menuClickHandler
 					} ]
 				}
 			}, {
 				text : 'Help'
 			} ]
-		}, {
-			flex : 1,
-			xtype : 'tabpanel',
-			activeTab : 0,
-			border : false,
-			items : [ {
-				title : 'Tab 1',
-				html : '<p>Center</p>'
-			}, {
-				title : 'Tab 2',
-				closable : true,
-				html : '<p>Center2</p>'
-			} ]
-		} ]
+		}, tabPanel ]
 	});
 };
 
-exampleApp.menuClickHandler = function(btn){
+exampleApp.menuClickHandler = function(btn) {
 	Ext.Msg.alert('Click', btn.text);
 }
