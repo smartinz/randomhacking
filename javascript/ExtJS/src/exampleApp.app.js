183ae932cba5c23d6d30fb9275534b630131c414
gianmarco.gherardi <devnull@localhost>
2010-02-18 22:16:19 +0100

    ExtJS

--- javascript/ExtJS/src/exampleApp.app.js
+++ javascript/ExtJS/src/exampleApp.app.js
@@ -1,50 +1,69 @@
 Ext.namespace('exampleApp');
 
 exampleApp.app = function() {
-	var tabPanel = new Ext.TabPanel( {
-		flex : 1,
-		activeTab : 0,
-		border : false,
-		items : [ {
-			title : 'Tab 1',
-			html : '<p>Center</p>'
-		}, {
-			title : 'Tab 2',
+	var viewport;
+	var tabPanel;
+
+	var newTab = function(btn) {
+		tabPanel.add( {
+			title : 'New Tab',
 			closable : true,
-			html : '<p>Center2</p>'
-		} ]
-	});
+			html : '<p>New Tab</p>'
+		});
+	};
 
-	new Ext.Viewport( {
-		layout : {
-			type : 'vbox',
-			pack : 'start',
-			align : 'stretch'
-		},
-		items : [ {
-			xtype : 'toolbar',
-			items : [ {
-				text : 'File',
-				menu : {
-					xtype : 'menu',
+	var showMsg = function(btn) {
+		Ext.Msg.alert('Click', btn.text);
+	};
+
+	return {
+		init : function() {
+			tabPanel = new Ext.TabPanel( {
+				flex : 1,
+				activeTab : 0,
+				border : false,
+				items : [ {
+					title : 'Tab 1',
+					html : '<p>Center</p>'
+				}, {
+					title : 'Tab 2',
+					closable : true,
+					html : '<p>Center2</p>'
+				} ]
+			});
+
+			viewport = new Ext.Viewport( {
+				layout : {
+					type : 'vbox',
+					pack : 'start',
+					align : 'stretch'
+				},
+				items : [ {
+					xtype : 'toolbar',
 					items : [ {
-						text : 'Open..',
-						handler : exampleApp.menuClickHandler
+						text : 'File',
+						menu : {
+							xtype : 'menu',
+							items : [ {
+								text : 'New Tab',
+								handler : newTab
+							}, {
+								text : 'Open..',
+								handler : showMsg
+							}, {
+								text : 'Save',
+								handler : showMsg
+							}, '-', {
+								text : 'Exit',
+								handler : showMsg
+							} ]
+						}
 					}, {
-						text : 'Save',
-						handler : exampleApp.menuClickHandler
-					}, '-', {
-						text : 'Exit',
-						handler : exampleApp.menuClickHandler
+						text : 'Help'
 					} ]
-				}
-			}, {
-				text : 'Help'
-			} ]
-		}, tabPanel ]
-	});
-};
+				}, tabPanel ]
+			});
+		}
+	};
+}();
 
-exampleApp.menuClickHandler = function(btn) {
-	Ext.Msg.alert('Click', btn.text);
-}
