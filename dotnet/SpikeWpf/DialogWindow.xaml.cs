76c16d27d2cf584dca1433f856e956bc38c829f4
gianmarco.gherardi <devnull@localhost>
2010-03-16 14:08:31 +0100

    SpikeWpf

--- dotnet/SpikeWpf/DialogWindow.xaml.cs
+++ dotnet/SpikeWpf/DialogWindow.xaml.cs
@@ -1,13 +1,19 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
 
 namespace SpikeWpf
 {
 	public partial class DialogWindow
 	{
+		static public ICommand CancelCommand = new RoutedCommand();
+		static public ICommand OkCommand = new RoutedCommand();
+
 		public DialogWindow()
 		{
 			InitializeComponent();
 		}
+
 
 		private void cancelButton_Click(object sender, RoutedEventArgs e)
 		{
@@ -17,6 +23,30 @@ namespace SpikeWpf
 		private void okButton_Click(object sender, RoutedEventArgs e)
 		{
 			DialogResult = true;
+		}
+
+		private void OkCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+			e.Handled = true;
+		}
+
+		private void OkCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			DialogResult = true;
+			e.Handled = true;
+		}
+
+		private void CancelCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			DialogResult = false;
+			e.Handled = true;
+		}
+
+		private void CancelCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = true;
+			e.Handled = true;
 		}
 	}
 }
