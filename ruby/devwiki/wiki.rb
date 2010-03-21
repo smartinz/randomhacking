089977107b00a4a2d5ab8953b8d3c3b5e4841dce
gianmarco.gherardi <devnull@localhost>
2010-03-21 18:28:16 +0100

    devwiki

--- ruby/devwiki/wiki.rb
+++ ruby/devwiki/wiki.rb
@@ -1,14 +1,17 @@
 require 'rubygems'
 require 'webrick'
 require 'kramdown'
+require 'pow' # http://github.com/probablycorey/pow
 
-module Wiki
+module DevWiki
+	FILE_EXTENSION = 'markdown'
+	
 	class WikiServlet < WEBrick::HTTPServlet::AbstractServlet
 		def do_GET(req, res)
 			file_path = File.join(@server[:DocumentRoot], req.path)
 			raise WEBrick::HTTPStatus::NotFound unless File.exist?(file_path)
 			res['content-type'] = 'text/html'
-			res.body = Wiki.to_html(file_path)
+			res.body = DevWiki.to_html(file_path)
 		end
 	end
 
@@ -21,12 +24,32 @@ module Wiki
 		html
 	end
 	
-	def self.export(target_dir, document_root = Dir.pwd)
-		
+	def self.export(source_dir, target_dir)
+		FileUtils.mkdir_p(target_dir) unless File.exist?(target_dir)
+		Dir.foreach(source_dir) do |child|
+			next if child == '.'
+			next if child == '..'
+
+			source_path = File.join(source_dir, child)
+			target_path = File.join(target_dir, child)
+
+			if File.file?(source_path)
+				file_extension = File.extname(source_path)[1..-1]
+				if file_extension == DevWiki::FILE_EXTENSION
+					File.open(target_path, 'w') do |f|
+						f.puts DevWiki.to_html(source_path)
+					end
+				else
+					FileUtils.cp source_path, target_path
+				end
+			else
+				DevWiki.export source_path, target_path
+			end
+		end
 	end
 
 	def self.start_server(port = 2000, document_root = Dir.pwd)
-		WEBrick::HTTPServlet::FileHandler.add_handler 'markdown', Wiki::WikiServlet
+		WEBrick::HTTPServlet::FileHandler.add_handler FILE_EXTENSION, DevWiki::WikiServlet
 		server = WEBrick::HTTPServer.new(:Port => port, :DocumentRoot => document_root)
 		['INT', 'TERM'].each do |signal|
 			trap(signal) { server.shutdown }
@@ -36,5 +59,6 @@ module Wiki
 end
 
 if $0 == __FILE__
-	Wiki.start_server
+	DevWiki.export File.dirname(__FILE__), 'c:/users/gimmi/temp/export'
+	DevWiki.start_server
 end
