2c112493e80d46345f8694c619b3979c9c171124
gianmarco.gherardi <devnull@localhost>
2010-03-23 13:48:32 +0100

    WindsorLoggingFacilitySpike

---
+++ dotnet/WindsorLoggingFacilitySpike/Test.cs
@@ -0,0 +1,20 @@
+﻿using Castle.Core.Logging;
+using Castle.Facilities.Logging;
+using Castle.Windsor;
+using NUnit.Framework;
+
+namespace WindsorLoggingFacilitySpike
+{
+	[TestFixture]
+	public class Test
+	{
+		[Test]
+		public void Works()
+		{
+			var ioc = new WindsorContainer();
+			ioc.AddFacility("logging", new LoggingFacility(LoggerImplementation.Log4net, "log4net.cfg.xml"));
+			var factory = ioc.Resolve<ILoggerFactory>();
+			factory.Create(typeof(Test)).Info("Works!");
+		}
+	}
+}
