855add2e84b8d87da8ba0ce807f98811920c5752
gianmarco.gherardi <devnull@localhost>
2010-03-31 18:07:33 +0200

    SpikeWcf

---
+++ dotnet/SpikeWcf/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace SpikeWcf
+{
+	[DataContract]
+	public class Customer
+	{
+		[DataMember]
+		public int Id { get; set; }
+
+		[DataMember]
+		public string Name { get; set; }
+	}
+}
---
+++ dotnet/SpikeWcf/CustomerService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace SpikeWcf
+{
+	[ServiceContract]
+	public class CustomerService
+	{
+		[OperationContract]
+		[WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "/GetAll")]
+		public Customer[] GetAll()
+		{
+			return new[]{
+				new Customer{ Id = 1, Name = "Uno" },
+				new Customer{ Id = 2, Name = "Due" },
+			};
+		}
+	}
+}
