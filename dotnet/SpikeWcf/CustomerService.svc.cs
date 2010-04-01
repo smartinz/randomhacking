3049bed9dbbdcac500cdef4294eb0c2b41046472
gianmarco.gherardi <devnull@localhost>
2010-04-01 10:42:51 +0200

    SpikeWcf

--- dotnet/SpikeWcf/CustomerService.svc.cs
+++ dotnet/SpikeWcf/CustomerService.svc.cs
@@ -7,13 +7,26 @@ namespace SpikeWcf
 	public class CustomerService
 	{
 		[OperationContract]
-		[WebInvoke(BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "/GetAll")]
-		public Customer[] GetAll()
+		[WebInvoke(BodyStyle = WebMessageBodyStyle.WrappedRequest, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "/GetAll")]
+		public Customer[] GetAll(Customer customer)
 		{
 			return new[]{
+				customer,
 				new Customer{ Id = 1, Name = "Uno" },
 				new Customer{ Id = 2, Name = "Due" },
 			};
 		}
+
+		//[OperationContract]
+		//[WebInvoke(BodyStyle = WebMessageBodyStyle.WrappedRequest, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "/GetAll")]
+		//public object JsonDataTypeTest(string stringPar, int intPar, bool boolPar, string[] arrayPar)
+		//{
+		//    return new{
+		//        stringPar,
+		//        intPar,
+		//        boolPar,
+		//        arrayPar,
+		//    };
+		//}
 	}
 }
