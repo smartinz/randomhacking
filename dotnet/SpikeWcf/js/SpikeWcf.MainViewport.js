de02213b9620551b417932933962a7ca12578666
gianmarco.gherardi <devnull@localhost>
2010-04-02 15:59:22 +0200

    SpikeWcf

--- dotnet/SpikeWcf/js/SpikeWcf.MainViewport.js
+++ dotnet/SpikeWcf/js/SpikeWcf.MainViewport.js
@@ -1,24 +1,28 @@
 ﻿"use strict";
+
 Ext.namespace("SpikeWcf");
 
 SpikeWcf.MainViewport = Ext.extend(Ext.Viewport, {
 	layout: 'fit',
 	hideBorders: true,
-	initComponent: function() {
-		this.items = [
-            {
-            	xtype: 'panel',
-            	tbar: {
-            		xtype: 'toolbar',
-            		items: [
-                        {
-                        	xtype: 'button',
-                        	text: 'Grid'
-                        }
-                    ]
-            	}
-            }
-        ];
+	initComponent: function () {
+		this.items = [{
+			xtype: 'panel',
+			tbar: {
+				xtype: 'toolbar',
+				items: [{
+					xtype: 'button',
+					text: 'Grid',
+					handler: this.gridButtonClick,
+					scope: this
+				}]
+			}
+		}];
 		SpikeWcf.MainViewport.superclass.initComponent.call(this);
+	},
+	
+	gridButtonClick: function () {
+		var window = new SpikeWcf.SearchWindow();
+		window.show();
 	}
 });
---
+++ dotnet/SpikeWcf/js/SpikeWcf.SearchWindow.js
@@ -0,0 +1,62 @@
+﻿"use strict";
+
+Ext.namespace("SpikeWcf");
+
+SpikeWcf.SearchWindow = Ext.extend(Ext.Window, {
+	title: 'Grid',
+	width: 300,
+	height: 300,
+	layout: 'vbox',
+	initComponent: function () {
+		this.layoutConfig = {
+			align: 'stretch',
+			pack: 'start'
+		};
+		this.items = [{
+			xtype: 'form',
+			labelWidth: 75,
+			layout: 'form',
+			border: false,
+			padding: 10,
+			items: [{
+				xtype: 'textfield',
+				fieldLabel: 'Text Filter',
+				anchor: '100%'
+			},
+			{
+				xtype: 'button',
+				text: 'MyButton'
+			}]
+		}/*,
+		{
+			xtype: 'grid',
+			flex: 1,
+			border: false,
+			columns: [{
+				xtype: 'gridcolumn',
+				header: 'Column',
+				sortable: true,
+				resizable: true,
+				width: 100,
+				dataIndex: 'string'
+			},
+			{
+				xtype: 'gridcolumn',
+				header: 'Column',
+				sortable: true,
+				resizable: true,
+				width: 100,
+				dataIndex: 'string'
+			},
+			{
+				xtype: 'gridcolumn',
+				header: 'Column',
+				sortable: true,
+				resizable: true,
+				width: 100,
+				dataIndex: 'string'
+			}]
+		}*/];
+		SpikeWcf.SearchWindow.superclass.initComponent.call(this);
+	}
+});
