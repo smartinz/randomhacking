1443e20f36051d760f914432e075526d945b6994
gianmarco.gherardi <devnull@localhost>
2010-04-07 11:15:55 +0200

    SpikeWcf

--- dotnet/SpikeWcf/CustomerService.svc.cs
+++ dotnet/SpikeWcf/CustomerService.svc.cs
@@ -19,12 +19,25 @@ namespace SpikeWcf
 		[WebInvoke(UriTemplate = "/Find", BodyStyle = WebMessageBodyStyle.WrappedRequest, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
 		public PagedItems<CustomerDto> Find(int start, int limit)
 		{
-			_log.DebugFormat("start: {0}, limit: {1}", start, limit);
+			_log.DebugFormat("Find(start: {0}, limit: {1})", start, limit);
 			using(var session = Global.SessionFactory.OpenSession())
 			{
 				var customers = session.Linq<Customer>().Skip(start).Take(limit);
 				var customerDtos = Mapper.Map<IEnumerable<Customer>, CustomerDto[]>(customers);
 				return new PagedItems<CustomerDto>(customerDtos, session.Linq<Customer>().Count());
+			}
+		}
+
+		[OperationContract]
+		[WebInvoke(UriTemplate = "/Get", BodyStyle = WebMessageBodyStyle.WrappedRequest, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+		public CustomerDto Get(string customerId)
+		{
+			_log.DebugFormat("Get(customerId: {0})", customerId);
+			using(var session = Global.SessionFactory.OpenSession())
+			{
+				var customer = session.Get<Customer>(customerId);
+				var customerDto = Mapper.Map<Customer, CustomerDto>(customer);
+				return customerDto;
 			}
 		}
 	}
--- dotnet/SpikeWcf/js/SpikeWcf.EditCustomerWindow.js
+++ dotnet/SpikeWcf/js/SpikeWcf.EditCustomerWindow.js
@@ -4,72 +4,75 @@ Ext.namespace("SpikeWcf");
 
 SpikeWcf.EditCustomerWindow = Ext.extend(Ext.Window, {
 	title: 'Edit Customer',
-	width: 650,
-	height: 390,
+	width: 300,
+	height: 300,
 	layout: 'fit',
 	initComponent: function() {
-		this.items = [
-            {
-            	xtype: 'form',
-            	labelWidth: 100,
-            	labelAlign: 'left',
-            	layout: 'form',
-            	border: false,
-            	padding: 10,
-            	items: [
-                    {
-                    	xtype: 'textfield',
-                    	fieldLabel: 'Company name',
-                    	anchor: '100%'
-                    },
-                    {
-                    	xtype: 'textfield',
-                    	fieldLabel: 'Contact name',
-                    	anchor: '100%'
-                    },
-                    {
-                    	xtype: 'textfield',
-                    	fieldLabel: 'Contact title',
-                    	anchor: '100%'
-                    },
-                    {
-                    	xtype: 'textfield',
-                    	fieldLabel: 'Address',
-                    	anchor: '100%'
-                    },
-                    {
-                    	xtype: 'textfield',
-                    	fieldLabel: 'City',
-                    	anchor: '100%'
-                    },
-                    {
-                    	xtype: 'textfield',
-                    	fieldLabel: 'Region',
-                    	anchor: '100%'
-                    },
-                    {
-                    	xtype: 'textfield',
-                    	fieldLabel: 'Postal code',
-                    	anchor: '100%'
-                    },
-                    {
-                    	xtype: 'textfield',
-                    	fieldLabel: 'Country',
-                    	anchor: '100%'
-                    },
-                    {
-                    	xtype: 'textfield',
-                    	fieldLabel: 'Phone',
-                    	anchor: '100%'
-                    },
-                    {
-                    	xtype: 'textfield',
-                    	fieldLabel: 'Fax',
-                    	anchor: '100%'
-                    }
-                ]
-            }
-        ];
+		this.editCustomerFormPanel = new Ext.form.FormPanel({
+			labelWidth: 100,
+			labelAlign: 'left',
+			layout: 'form',
+			border: false,
+			padding: 10,
+			buttons: [{
+				text: 'Load',
+				handler: function () {
+					alert('ok');
+				}
+			}],
+			items: [{
+				xtype: 'textfield',
+				fieldLabel: 'Company name',
+				anchor: '100%'
+			},
+			{
+				xtype: 'textfield',
+				fieldLabel: 'Contact name',
+				anchor: '100%'
+			},
+			{
+				xtype: 'textfield',
+				fieldLabel: 'Contact title',
+				anchor: '100%'
+			},
+			{
+				xtype: 'textfield',
+				fieldLabel: 'Address',
+				anchor: '100%'
+			},
+			{
+				xtype: 'textfield',
+				fieldLabel: 'City',
+				anchor: '100%'
+			},
+			{
+				xtype: 'textfield',
+				fieldLabel: 'Region',
+				anchor: '100%'
+			},
+			{
+				xtype: 'textfield',
+				fieldLabel: 'Postal code',
+				anchor: '100%'
+			},
+			{
+				xtype: 'textfield',
+				fieldLabel: 'Country',
+				anchor: '100%'
+			},
+			{
+				xtype: 'textfield',
+				fieldLabel: 'Phone',
+				anchor: '100%'
+			},
+			{
+				xtype: 'textfield',
+				fieldLabel: 'Fax',
+				anchor: '100%'
+			}]
+		});
+
+		this.items = [this.editCustomerFormPanel];
 		SpikeWcf.EditCustomerWindow.superclass.initComponent.call(this);
 	}
 });
