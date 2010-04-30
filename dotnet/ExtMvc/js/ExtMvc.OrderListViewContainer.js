205772077958f2d7c6d4a9cd3b8c1cc68515fbae
gianmarco.gherardi <devnull@localhost>
2010-04-30 17:35:53 +0200

    ExtMvc

---
+++ dotnet/ExtMvc/js/ExtMvc.OrderListViewContainer.js
@@ -0,0 +1,72 @@
+﻿"use strict";
+
+Ext.namespace("ExtMvc");
+
+ExtMvc.OrderListViewContainer = Ext.extend(Ext.Container, {
+	layout: 'fit',
+	initComponent: function () {
+		this.gridPanel =  this.buildGridPanel(this.dataProxy);
+		this.items = this.gridPanel;
+
+		this.addEvents('itemselected');
+
+		this.gridPanel.on('rowdblclick', this.gridPanel_rowDblClick, this);
+
+		ExtMvc.OrderListViewContainer.superclass.initComponent.call(this);
+	},
+
+	gridPanel_rowDblClick: function (grid, rowIndex, event) {
+		var item = grid.getStore().getAt(rowIndex).data;
+		this.fireEvent('itemselected', this, item);
+	},
+
+	loadItems: function (args) {
+		this.gridPanel.getStore().load({
+			params: Ext.apply({
+				start: 0,
+				limit: this.gridPanel.getBottomToolbar().pageSize
+			}, args)
+		});
+	},
+
+	buildGridPanel: function (dataProxy) {
+		var store = new Ext.data.Store({
+			proxy: dataProxy,
+			remoteSort: true,
+			reader: new Ext.data.JsonReader({
+				root: 'items',
+				idProperty: "OrderId",
+				totalProperty: 'count',
+				fields: ['OrderId', 'OrderDate', 'RequiredDate', 'ShippedDate', 'Freight', 'ShipName', 'ShipAddress', 'ShipCity', 'ShipRegion', 'ShipPostalCode', 'ShipCountry']
+			})
+		});
+
+		return new Ext.grid.GridPanel({
+			border: false,
+			store: store,
+			colModel: new Ext.grid.ColumnModel({
+				defaults: { width: 60, sortable: true },
+				columns: [
+					{ dataIndex: 'OrderId', header: "OrderId" },
+					{ dataIndex: 'OrderDate', header: "OrderDate" },
+					{ dataIndex: 'RequiredDate', header: "RequiredDate" },
+					{ dataIndex: 'ShippedDate', header: "ShippedDate" },
+					{ dataIndex: 'Freight', header: "Freight" },
+					{ dataIndex: 'ShipName', header: "ShipName" },
+					{ dataIndex: 'ShipAddress', header: "ShipAddress" },
+					{ dataIndex: 'ShipCity', header: "ShipCity" },
+					{ dataIndex: 'ShipRegion', header: "ShipRegion" },
+					{ dataIndex: 'ShipPostalCode', header: "ShipPostalCode" },
+					{ dataIndex: 'ShipCountry', header: "ShipCountry" }
+				]
+			}),
+
+			bbar: new Ext.PagingToolbar({
+				store: store,
+				displayInfo: true,
+				pageSize: 25,
+				prependButtons: true
+			})
+		});
+	}
+});
