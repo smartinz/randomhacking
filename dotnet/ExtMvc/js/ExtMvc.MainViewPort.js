9bc733333ea15ccb440fc4e413cce3f0098b7598
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2010-05-28 18:35:18 +0200

    ExtMvc

--- dotnet/ExtMvc/js/ExtMvc.MainViewPort.js
+++ dotnet/ExtMvc/js/ExtMvc.MainViewPort.js
@@ -2,7 +2,7 @@
 /*global Ext, ExtMvc */
 "use strict";
 
-Ext.namespace("ExtMvc");
+Ext.namespace('ExtMvc');
 
 ExtMvc.MainViewport = Ext.extend(Ext.Viewport, {
 	layout: 'fit',
@@ -13,63 +13,238 @@ ExtMvc.MainViewport = Ext.extend(Ext.Viewport, {
 			tbar: {
 				xtype: 'toolbar',
 				items: [{
-					xtype: 'button',
-					text: 'Search Customer',
-					handler: this.searchCustomerClick,
-					scope: this
+					text: 'Category',
+					menu: {
+						items: [{
+							text: 'Search ',
+							handler: this.searchCategoryClick,
+							scope: this
+						}]
+					}
 				}, {
-					xtype: 'button',
-					text: 'Edit Customer',
-					handler: this.editCustomerClick,
-					scope: this
+					text: 'CustomerDemographic',
+					menu: {
+						items: [{
+							text: 'Search ',
+							handler: this.searchCustomerDemographicClick,
+							scope: this
+						}]
+					}
 				}, {
-					xtype: 'button',
-					text: 'Test',
-					handler: this.testClick,
-					scope: this
+					text: 'Customer',
+					menu: {
+						items: [{
+							text: 'Search ',
+							handler: this.searchCustomerClick,
+							scope: this
+						}]
+					}
+				}, {
+					text: 'Employee',
+					menu: {
+						items: [{
+							text: 'Search ',
+							handler: this.searchEmployeeClick,
+							scope: this
+						}]
+					}
+				}, {
+					text: 'OrderDetail',
+					menu: {
+						items: [{
+							text: 'Search ',
+							handler: this.searchOrderDetailClick,
+							scope: this
+						}]
+					}
+				}, {
+					text: 'Order',
+					menu: {
+						items: [{
+							text: 'Search ',
+							handler: this.searchOrderClick,
+							scope: this
+						}]
+					}
+				}, {
+					text: 'Product',
+					menu: {
+						items: [{
+							text: 'Search ',
+							handler: this.searchProductClick,
+							scope: this
+						}]
+					}
+				}, {
+					text: 'Region',
+					menu: {
+						items: [{
+							text: 'Search ',
+							handler: this.searchRegionClick,
+							scope: this
+						}]
+					}
+				}, {
+					text: 'Shipper',
+					menu: {
+						items: [{
+							text: 'Search ',
+							handler: this.searchShipperClick,
+							scope: this
+						}]
+					}
+				}, {
+					text: 'Supplier',
+					menu: {
+						items: [{
+							text: 'Search ',
+							handler: this.searchSupplierClick,
+							scope: this
+						}]
+					}
+				}, {
+					text: 'Sysdiagram',
+					menu: {
+						items: [{
+							text: 'Search ',
+							handler: this.searchSysdiagramClick,
+							scope: this
+						}]
+					}
+				}, {
+					text: 'Territory',
+					menu: {
+						items: [{
+							text: 'Search ',
+							handler: this.searchTerritoryClick,
+							scope: this
+						}]
+					}
 				}]
 			}
 		}];
 		ExtMvc.MainViewport.superclass.initComponent.call(this);
 	},
-
-	searchCustomerClick: function () {
-		var window = new Ext.Window({
-			title: 'Search Customer',
-			width: 600,
-			height: 300,
-			layout: 'fit',
-			items: new ExtMvc.CustomerSearchContainer()
-		});
-		window.show();
-	},
-
-	editCustomerClick: function () {
-		var window = new Ext.Window({
-			title: 'Edit Customer',
-			width: 300,
-			height: 300,
-			layout: 'fit',
-			maximizable: true,
-			items: new ExtMvc.CustomerFormPanel()
-		});
-		window.show();
-	},
-
-	testClick: function () {
-/*		var obj, lvc, window;
-		obj = JSON.parse('{"OrderId":10643,"OrderDate":"\/Date(872460000000)\/","RequiredDate":"\/Date(874879200000)\/","ShippedDate":"\/Date(873151200000)\/","Freight":29.4600,"ShipName":"Alfreds Futterkiste","ShipAddress":"Obere Str. 57","ShipCity":"Berlin","ShipRegion":null,"ShipPostalCode":"12209","ShipCountry":"Germany"}');
-		lvc = new ExtMvc.OrderListViewContainer({
-			dataProxy: new Ext.data.MemoryProxy({ items: obj })
-		});
-		window = new Ext.Window({
-			title: 'Test',
-			width: 600,
-			height: 300,
-			layout: 'fit',
-			items: lvc
-		});
-		lvc.loadItems({});
-		window.show();*/
-	}
+	
+	searchCategoryClick: function () {
+			var window = new Ext.Window({
+				title: 'Search Category ',
+				width: 300,
+				height: 300,
+				layout: 'fit',
+				maximizable: true,
+				items: new ExtMvc.CategorySearchContainer()
+			});
+			window.show();
+		}, searchCustomerDemographicClick: function () {
+			var window = new Ext.Window({
+				title: 'Search CustomerDemographic ',
+				width: 300,
+				height: 300,
+				layout: 'fit',
+				maximizable: true,
+				items: new ExtMvc.CustomerDemographicSearchContainer()
+			});
+			window.show();
+		}, searchCustomerClick: function () {
+			var window = new Ext.Window({
+				title: 'Search Customer ',
+				width: 300,
+				height: 300,
+				layout: 'fit',
+				maximizable: true,
+				items: new ExtMvc.CustomerSearchContainer()
+			});
+			window.show();
+		}, searchEmployeeClick: function () {
+			var window = new Ext.Window({
+				title: 'Search Employee ',
+				width: 300,
+				height: 300,
+				layout: 'fit',
+				maximizable: true,
+				items: new ExtMvc.EmployeeSearchContainer()
+			});
+			window.show();
+		}, searchOrderDetailClick: function () {
+			var window = new Ext.Window({
+				title: 'Search OrderDetail ',
+				width: 300,
+				height: 300,
+				layout: 'fit',
+				maximizable: true,
+				items: new ExtMvc.OrderDetailSearchContainer()
+			});
+			window.show();
+		}, searchOrderClick: function () {
+			var window = new Ext.Window({
+				title: 'Search Order ',
+				width: 300,
+				height: 300,
+				layout: 'fit',
+				maximizable: true,
+				items: new ExtMvc.OrderSearchContainer()
+			});
+			window.show();
+		}, searchProductClick: function () {
+			var window = new Ext.Window({
+				title: 'Search Product ',
+				width: 300,
+				height: 300,
+				layout: 'fit',
+				maximizable: true,
+				items: new ExtMvc.ProductSearchContainer()
+			});
+			window.show();
+		}, searchRegionClick: function () {
+			var window = new Ext.Window({
+				title: 'Search Region ',
+				width: 300,
+				height: 300,
+				layout: 'fit',
+				maximizable: true,
+				items: new ExtMvc.RegionSearchContainer()
+			});
+			window.show();
+		}, searchShipperClick: function () {
+			var window = new Ext.Window({
+				title: 'Search Shipper ',
+				width: 300,
+				height: 300,
+				layout: 'fit',
+				maximizable: true,
+				items: new ExtMvc.ShipperSearchContainer()
+			});
+			window.show();
+		}, searchSupplierClick: function () {
+			var window = new Ext.Window({
+				title: 'Search Supplier ',
+				width: 300,
+				height: 300,
+				layout: 'fit',
+				maximizable: true,
+				items: new ExtMvc.SupplierSearchContainer()
+			});
+			window.show();
+		}, searchSysdiagramClick: function () {
+			var window = new Ext.Window({
+				title: 'Search Sysdiagram ',
+				width: 300,
+				height: 300,
+				layout: 'fit',
+				maximizable: true,
+				items: new ExtMvc.SysdiagramSearchContainer()
+			});
+			window.show();
+		}, searchTerritoryClick: function () {
+			var window = new Ext.Window({
+				title: 'Search Territory ',
+				width: 300,
+				height: 300,
+				layout: 'fit',
+				maximizable: true,
+				items: new ExtMvc.TerritorySearchContainer()
+			});
+			window.show();
+		}
 });
