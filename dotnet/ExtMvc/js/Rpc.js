e79cb84b9e6872806570f919f2271c18c3262552
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2010-06-01 18:44:41 +0200

    ExtMvc

--- dotnet/ExtMvc/js/Rpc.js
+++ dotnet/ExtMvc/js/Rpc.js
@@ -3,21 +3,6 @@
 "use strict";
 
 Rpc = {
-	/*
-	init: function () {
-	Ext.Ajax.on('requestexception', function (conn, response, options) {
-	if (!Ext.MessageBox.isVisible()) {
-	Ext.MessageBox.show({
-	title: 'Error',
-	msg: 'An error occured',
-	buttons: Ext.MessageBox.OK,
-	icon: Ext.MessageBox.ERROR
-	});
-	}
-	});
-	},
-	*/
-
 	call: function (opts) {
 		var reviver = function (key, value) {
 			if (typeof value === 'string') {
@@ -66,66 +51,20 @@ Rpc = {
 			};
 			Rpc.JsonPostHttpProxy.superclass.doRequest.call(this, action, rs, params, reader, cb, scope, arg);
 		}
-	})/*,
-
-	// For use in Ext.form.FormPanel, example:
-	// this.getForm().doAction(new Rpc.JsonLoadFormAction(this.getForm(), { url: '/Customer/Get', params: { id: 'ALFKI' }, waitMsg: 'Loading...' }));
-	JsonLoadFormAction: Ext.extend(Ext.form.Action.Load, {
-		type: 'jsonload',
-		run: function () {
-			Ext.Ajax.request(Ext.apply(this.createCallback(this.options), {
-				method: this.getMethod(),
-				url: this.getUrl(false),
-				headers: this.options.headers,
-				//params:this.getParams()
-				jsonData: JSON.stringify(Ext.applyIf(this.options.params || {}, this.form.baseParams))
-			}));
-		}
 	}),
 
-	// For use in Ext.form.FormPanel, example:
-	// this.getForm().doAction(new Rpc.JsonSubmitFormAction(this.getForm(), { url: '/Customer/Update',	waitMsg: 'Saving...' }));
-	JsonSubmitFormAction: Ext.extend(Ext.form.Action.Submit, {
-		type: 'jsonsubmit',
-		run: function () {
-			var o = this.options,
-            method = this.getMethod(),
-            isGet = method == 'GET';
-			if (o.clientValidation === false || this.form.isValid()) {
-				if (o.submitEmptyText === false) {
-					var fields = this.form.items,
-                    emptyFields = [];
-					fields.each(function (f) {
-						if (f.el.getValue() == f.emptyText) {
-							emptyFields.push(f);
-							f.el.dom.value = "";
-						}
-					});
-				}
-				Ext.Ajax.request(Ext.apply(this.createCallback(o), {
-					//form: this.form.el.dom,
-					url: this.getUrl(isGet),
-					method: method,
-					headers: o.headers,
-					//params: !isGet ? this.getParams() : null,
-					jsonData: JSON.stringify(Ext.apply({
-						item: this.form.getFieldValues()
-					}, !isGet ? Ext.applyIf(this.options.params || {}, this.form.baseParams) : {})),
-					isUpload: this.form.fileUpload
-				}));
-				if (o.submitEmptyText === false) {
-					Ext.each(emptyFields, function (f) {
-						if (f.applyEmptyText) {
-							f.applyEmptyText();
-						}
-					});
-				}
-			} else if (o.clientValidation !== false) { // client validation failed
-				this.failureType = Ext.form.Action.CLIENT_INVALID;
-				this.form.afterAction(this, false);
+	/*
+	// TODO Work in progress
+	JsonReader: Ext.extend(Ext.data.JsonReader, {
+		read: function (response) {
+			var json = response.responseText;
+			var o = Ext.decode(json);
+			if (!o) {
+				throw { message: 'JsonReader.read: Json object not found' };
 			}
+			return this.readRecords(o);
 		}
 	})
-*/
+	*/
 };
 
